596aa6ce80e8f1c20831cc4f4684f1ce6ec600cf
Germano <124100799+JoaoGerman@users.noreply.github.com>
2024-04-12 21:41:45 -0300

    tala minha criança

---
+++ LaçosDeRepetição/Atividade_Repetiçao07.java
@@ -0,0 +1,8 @@
+package com.example;
+public class Atividade_Repetiçao07 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+    }
+}
+}
