2c3f6c32bf034bcb93437e307c06f2ee723e9b6e
JoaoGerman <joaogermano2730@gmail.com>
2024-08-08 15:21:42 -0300

    beta 1

--- LaçosDeRepetição/Atividade_Repetiçao07.java
+++
@@ -1,8 +0,0 @@
-package com.example;
-public class Atividade_Repetiçao07 {
-    public static void main(String[] args) {
-        for (int i = 1; i <= 10; i++) {
-            System.out.println(i);
-    }
-}
-}
--- LaçosDeRepetição/Atividade_Repetiçao08.java
+++
@@ -1,8 +0,0 @@
-package com.example;
-public class Atividade_Repetiçao08 {
-    public static void main(String[] args) {
-        for (int i = 10; i >= 1; i--) {
-            System.out.println(i);
-        }
-    }
-}
